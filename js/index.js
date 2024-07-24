4a32f44963bbc36ad8b63972cdc93822c5261043
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-07-24 20:57:06 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,87 @@
+//1
+// const number = [1, 2, 3, 4, 5];
+
+// const res = [];
+// for (let i = 0; i < number.length; i++) {
+//   res.push(number[i] * 2);
+// }
+
+// console.log(res);
+
+//2
+// const number = [1, 2, 3, 4, 5];
+
+// const res = [];
+// for (let i = number.length - 1; i >= 0; i--) {
+//   res.push(number[i]);
+// }
+
+// console.log(res);
+
+//3
+// const numbers = [-1, 1, 2, -2, 3, -3, 4, 5];
+
+// const res = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] >= 0) {
+//     res.push(numbers[i]);
+//   }
+// }
+
+// console.log(res);
+
+//4
+// const numbers = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log(sum);
+
+//5
+// const numbers = [1, 2, 3, 4, 5];
+
+// numbers.push(6);
+
+// console.log(numbers);
+
+//6
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.pop()
+
+// console.log(numbers)
+
+//7
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.shift()
+
+// console.log(numbers)
+
+//8
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.unshift(0)
+
+// console.log(numbers)
+
+//9
+// const numbers = [1, 2, 3, 4, 5];
+// const izlash = 3;
+
+// const index = numbers.indexOf(izlash);
+
+// console.log(index);
+
+//10
+// const numbers = [1, 2, 3, 4, 5];
+// const massiv = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   massiv.push(numbers[i]);
+// }
+
+// console.log(massiv);
