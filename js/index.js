897d339a6233613c4181abc3eaa060915e7074bb
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-07-24 21:11:06 +0500

    none

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//1
+// 1
 // const number = [1, 2, 3, 4, 5];
 
 // const res = [];
@@ -6,9 +6,7 @@
 //   res.push(number[i] * 2);
 // }
 
-// console.log(res);
-
-//2
+// 2
 // const number = [1, 2, 3, 4, 5];
 
 // const res = [];
@@ -16,9 +14,7 @@
 //   res.push(number[i]);
 // }
 
-// console.log(res);
-
-//3
+// 3
 // const numbers = [-1, 1, 2, -2, 3, -3, 4, 5];
 
 // const res = [];
@@ -28,9 +24,7 @@
 //   }
 // }
 
-// console.log(res);
-
-//4
+// 4
 // const numbers = [1, 2, 3, 4, 5];
 
 // let sum = 0;
@@ -38,45 +32,33 @@
 //   sum += numbers[i];
 // }
 
-// console.log(sum);
-
-//5
+// 5
 // const numbers = [1, 2, 3, 4, 5];
 
 // numbers.push(6);
 
-// console.log(numbers);
-
-//6
+// 6
 // const numbers = [1, 2, 3, 4, 5]
 
 // numbers.pop()
 
-// console.log(numbers)
-
-//7
+// 7
 // const numbers = [1, 2, 3, 4, 5]
 
 // numbers.shift()
 
-// console.log(numbers)
-
-//8
+// 8
 // const numbers = [1, 2, 3, 4, 5]
 
 // numbers.unshift(0)
 
-// console.log(numbers)
-
-//9
+// 9
 // const numbers = [1, 2, 3, 4, 5];
 // const izlash = 3;
 
 // const index = numbers.indexOf(izlash);
 
-// console.log(index);
-
-//10
+// 10
 // const numbers = [1, 2, 3, 4, 5];
 // const massiv = [];
 
@@ -84,4 +66,123 @@
 //   massiv.push(numbers[i]);
 // }
 
-// console.log(massiv);
+// 11
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+// let engKatta = massiv[0];
+// for (let i = 1; i < massiv.length; i++) {
+//   if (massiv[i] > engKatta) {
+//     engKatta = massiv[i];
+//   }
+// }
+
+// 12
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let engKichik = massiv[0];
+// for (let i = 1; i < massiv.length; i++) {
+//   if (massiv[i] < engKichik) {
+//     engKichik = massiv[i];
+//   }
+// }
+
+// 13
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let juftSonlar = 0;
+// for (let i = 0; i < massiv.length; i++) {
+//   if (massiv[i] % 2 === 0) {
+//     juftSonlar++;
+//   }
+// }
+
+// 14
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let toqSonlar = 0;
+// for (let i = 0; i < massiv.length; i++) {
+//   if (massiv[i] % 2 !== 0) {
+//     toqSonlar++;
+//   }
+// }
+
+// 15
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+// let yangiMassiv = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   if (massiv[i] !== 0) {
+//     yangiMassiv.push(massiv[i]);
+//   }
+// }
+
+// 16
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+// let ikkilanganMassiv = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   ikkilanganMassiv.push(massiv[i]);
+//   ikkilanganMassiv.push(massiv[i]);
+// }
+
+// 17
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let birinchiQism = [];
+// let ikkinchiQism = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   if (i % 2 === 0) {
+//     birinchiQism.push(massiv[i]);
+//   } else {
+//     ikkinchiQism.push(massiv[i]);
+//   }
+// }
+
+// 18
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let kopaytma = 1;
+// for (let i = 0; i < massiv.length; i++) {
+//   kopaytma *= massiv[i];
+// }
+
+// 19
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let toqIndeksli = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   if (i % 2 !== 0) {
+//     toqIndeksli.push(massiv[i]);
+//   }
+// }
+
+// 20
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let juftIndeksli = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   if (i % 2 === 0) {
+//     juftIndeksli.push(massiv[i]);
+//   }
+// }
+
+// 21
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+// let stringElementlar = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   if (typeof massiv[i] === "string") {
+//     stringElementlar.push(massiv[i]);
+//   }
+// }
+
+// 22
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let numberElementlar = [];
+// for (let i = 0; i < massiv.length; i++) {
+//   if (typeof massiv[i] === "number") {
+//     numberElementlar.push(massiv[i]);
+//   }
+// }
+
+// 23
+// let massiv = [3, 7, 2, 8, 1, 5, 4];
+
+// let massivUzunligi = massiv.length;
